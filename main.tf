df6ce0c8637fe50b49b32dd35d5ac325088e6f41
Pratik Khasnabis <pratik.khasnabis@outlook.com>
2022-04-08 12:59:52 +1000

    Changed queue name to a variable

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ provider "aws" {
 }
 
 resource "aws_sqs_queue" "terraform_queue" {
-  name                      = "tfe-example-queue"
+  name                      = var.queue_name
   delay_seconds             = 90
   max_message_size          = 2048
   message_retention_seconds = 86400
