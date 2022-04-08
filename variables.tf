7326d3ad8771b1c1baf2d7edeed6e6fac30279e9
Pratik Khasnabis <pratik.khasnabis@outlook.com>
2022-04-08 15:39:10 +1000

    Added more variables for AWS provider

--- variables.tf
+++ variables.tf
@@ -2,3 +2,19 @@ variable "queue_name" {
   type        = string
   description = "The name of the SQS queue."
 }
+
+variable "account_id" {
+  type        = string
+  description = "The ARN of target AWS account."
+}
+
+variable "role" {
+  type        = string
+  description = "The role name in target AWS account."
+}
+
+variable "region" {
+  type        = string
+  type        = "us-east-1"
+  description = "The region to deploy in target AWS account."
+}
