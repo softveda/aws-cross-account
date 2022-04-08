ce69e614179201f767e29141c523fdba84d36509
Pratik Khasnabis <pratik.khasnabis@outlook.com>
2022-04-08 11:35:29 +1000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.20.0"
+    }
+  }
+
+  required_version = ">= 0.15"
+}
+provider "aws" {
+  region  = "us-east-1"
+  //profile = "source"
+
+  assume_role {
+    role_arn = "arn:aws:iam::716283266746:role/hc-dynamodb-ro"
+  }
+}
+
+resource "aws_sqs_queue" "terraform_queue" {
+  name                      = "tfe-example-queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+output "SQS-URL" {
+	value = aws_sqs_queue.terraform_queue.id
+}
